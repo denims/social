2441ea4faa718036c03c5a0a92db2941fe3c0cd7
denims <denimallel@gmail.com>
2024-09-19 13:33:36 +0530

    Added new post

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,17 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 function PostList() {
   return (
-    <ul className={classes.posts}>
-      <Post name="Deni" msg="hello" />
-      <Post name="Deni2" msg="hello2" />
-      <Post name="Deni3" msg="hello3" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post name="Deni" msg="hello" />
+        <Post name="Deni2" msg="hello2" />
+        <Post name="Deni3" msg="hello3" />
+      </ul>
+    </>
   );
 }
 
